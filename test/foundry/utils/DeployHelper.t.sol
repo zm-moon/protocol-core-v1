881471ee7c9220de42967bd20f10f93c3a7bfaf3
Raul <raul.mf86@gmail.com>
2024-03-18 22:28:29 -0300

    use TestProxyHelper in tests instead of upgrades

--- test/foundry/utils/DeployHelper.t.sol
+++ test/foundry/utils/DeployHelper.t.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.23;
 // external
 import { console2 } from "forge-std/console2.sol"; // console to indicate mock deployment calls.
 import { ERC6551Registry } from "erc6551/ERC6551Registry.sol";
-import { Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import { TestProxyHelper } from "./TestProxyHelper.sol";
 
 // contracts
 import { AccessController } from "../../../contracts/AccessController.sol";
