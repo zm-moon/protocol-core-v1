9cc03d21451b15a085401ec2cc4ad7496b46a3c1
Spablob <99089658+Spablob@users.noreply.github.com>
2024-12-10 14:10:51 +0000

    fix file licenses (#333)

--- contracts/modules/royalty/policies/VaultController.sol
+++ contracts/modules/royalty/policies/VaultController.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.26;
 
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
